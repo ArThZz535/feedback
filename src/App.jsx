7d7e28180ebd8f47fd3232f9425e52310f003661
Arthur Rodrigues de Sousa Araújo <arthur.rodriguess.sousa@gmail.com>
2024-06-11 00:06:12 +0000

    style: criação dos estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "styled-components"
 
 import { Home } from "./pages/Home"
 import { theme } from "./styles/theme.js"
+import { GlobalStyle } from "./styles/global.js"
 
 const router = createBrowserRouter([
 
@@ -19,6 +20,7 @@ export function App() {
 
     <ThemeProvider theme={theme}>
       <RouterProvider router={router}/>
+      <GlobalStyle />
     </ThemeProvider>
 
   )
