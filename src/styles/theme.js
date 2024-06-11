a553c5eb8389746c5c3406e3de98bdc4009d9c10
Arthur Rodrigues de Sousa Araújo <arthur.rodriguess.sousa@gmail.com>
2024-06-11 00:36:45 +0000

    styled: estilização do h1 e do p do card

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,7 +3,7 @@ export const theme = {
     cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
     textColor: "#969FAD",
     submitButtonColor: "#FC761",
-    white: "FFF",
+    white: "#FFF",
     feedbackButtonColor: "#262E38",
     feedbackButtonText: "#7C8798"
 }
